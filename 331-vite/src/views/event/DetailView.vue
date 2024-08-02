1f01c28d31d8ebd9f5067215bf46eb6b70d284be
Ruth <hyangdan0715@gmail.com>
2024-08-02 10:10:41 +0700

    done lab3#4

---
+++ 331-vite/src/views/event/DetailView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { toRefs, defineProps } from 'vue'
+  import {type Event} from '@/types'
+  
+
+  const props=defineProps<{
+    event:Event,
+    id:String
+}>()
+const {event}= toRefs(props);
+</script>
+
+<template>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+</template>
