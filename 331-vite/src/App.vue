a3c65db1c138a2bcbacbd2fd8fbd6b93c2c1c3ca
Ruth <hyangdan0715@gmail.com>
2024-08-14 17:12:06 +0700

    done whole lab5

--- 331-vite/src/App.vue
+++ 331-vite/src/App.vue
@@ -3,6 +3,7 @@ import {ref} from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from './stores/message'
 import { storeToRefs } from 'pinia'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const store = useMessageStore()
 const { message } = storeToRefs(store)
@@ -10,6 +11,7 @@ const pageSize = ref(2);
 </script>
 
 <template>
+  <SpeedInsights />
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
